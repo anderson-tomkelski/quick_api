b86efe13d2307680592a2efa0a4456bd3679c0ee
anderson-tomkelski <ander.tomkelski@gmail.com>
2020-02-11 14:12:46 -0300

    Meeting ref id added on create time

--- app/Http/Controllers/PlanoAcaoController.php
+++ app/Http/Controllers/PlanoAcaoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\PlanoAcao;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class PlanoAcaoController extends Controller
 {
 
@@ -15,7 +17,21 @@ class PlanoAcaoController extends Controller
 
     public function create(Request $request)
     {
-        $planoAcao = PlanoAcao::create($request->all());
+        $data = $request->all();
+        $idReuniao = $data['id_reuniao'];
+        
+        $results = DB::select(
+            "SELECT * FROM reuniao_realizada
+                WHERE id_reuniao = " . $idReuniao . "
+                ORDER BY id DESC LIMIT 1
+            "
+        );
+
+        $data['id_reuniao_ref'] = $results[0]->id;
+        $data['datacriado'] = date("Y-m-d H:i:s");
+
+
+        $planoAcao = PlanoAcao::create($data);
 
         return response()->json($planoAcao, 201);
     }
